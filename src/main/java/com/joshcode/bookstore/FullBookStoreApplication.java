eabc47051e465608ee24fbd6e9f99a66621fe94a
Josh <rollingmoney2015@gmail.com>
2025-06-10 23:24:01 +0100

    first commit

---
+++ src/main/java/com/joshcode/bookstore/FullBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.joshcode.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshcode/bookstore/FullBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshcode.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
