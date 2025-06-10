01e0f458e074b9d666eb20f2a6c5843f70af5173
Josh <rollingmoney2015@gmail.com>
2025-06-11 00:48:59 +0100

    added enableAuditing

--- src/main/java/com/joshcode/bookstore/FullBookStoreApplication.java
+++ src/main/java/com/joshcode/bookstore/FullBookStoreApplication.java
@@ -2,8 +2,10 @@ package com.joshcode.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FullBookStoreApplication {
 
 	public static void main(String[] args) {
